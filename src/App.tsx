4741c74b7bed19cd5ecea2884ae40d777cb8be84
Sagy Gideon Mizrahi <sagy26@gmail.com>
2024-07-14 12:16:01 +0300

    final cleanup

--- src/App.tsx
+++ src/App.tsx
@@ -5,16 +5,8 @@ import Navbar from './components/Navbar/Navbar';
 import Home from './pages/Home/Home';
 import Catalogue from './pages/Catalogue/Catalogue';
 import Filter from './pages/Filter/Filter';
-import './App.css';
 
 const App: React.FC = () => {
-  // todo:
-  // 1. Refactor to remove the pages and views. They should be in a single file each
-  // 2. Update the navbar link itmes
-  // 3. Add a toggle between list and grid views for the catlogue
-  // 4. Review and remove the redundant files and lines
-  // 5. Write the decisions considerations
-  // 6. Upload the repo to github
   return (
     <PeopleProvider>
       <Router>
